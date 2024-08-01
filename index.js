0769e4ed9023690e1072f13eb6addbef0cd893bf
Tazwar Saif <tazwarsaif02@gmail.com>
2024-08-01 14:20:16 +0600

    iii

---
+++ index.js
@@ -0,0 +1,27 @@
+import bodyParser from 'body-parser';
+import express from 'express';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const app = express();
+const port = 3000;
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname+'/public/index.html');
+})
+
+app.post('/check',(req,res)=>{
+    if(req.body['password']==='imranimran'){
+        res.sendFile(__dirname+"/public/secret.html");
+    }
+    else{
+        res.sendFile(__dirname+'/public/index.html');
+    }
+})
+
+app.listen(port,()=>{
+    console.log(`Listening to Port ${port}...`);
+})
